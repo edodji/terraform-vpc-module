f955d030b7c3f017c4ce5ebe19bd73f6dc151221
Kokouvi Eklou <edodji02@gmail.com>
2025-04-21 14:43:15 -0600

    vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.project_name}-${environment}-vpc"
+    Name = "${var.project_name}-${var.environment}-vpc"
   }
 }
 
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name = "${var.project_name}-${environment}-igw"
+    Name = "${var.project_name}-${var.environment}-igw"
   }
 }
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "public_subnet_az1" {
   map_public_ip_on_launch =true
 
   tags = {
-    Name = "${var.project_name}-${environment}-public-az1"
+    Name = "${var.project_name}-${var.environment}-public-az1"
   }
 }
 
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_subnet_az2" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.project_name}-${environment}-public-az2"
+    Name = "${var.project_name}-${var.environment}-public-az2"
   }
 }
 
@@ -55,7 +55,7 @@ resource "aws_route_table" "public_route_table" {
   }
 
   tags = {
-    Name = "${var.project_name}-${environment}-public-rt"
+    Name = "${var.project_name}-${var.environment}-public-rt"
   }
 }
 
@@ -79,7 +79,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.project_name}-${environment}-private-app-az1"
+    Name = "${var.project_name}-${var.environment}-private-app-az1"
   }
 }
 
@@ -91,7 +91,7 @@ resource "aws_subnet" "private_app_subnet_az2" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.project_name}-${environment}-private-app-az2"
+    Name = "${var.project_name}-${var.environment}-private-app-az2"
   }
 }
 
@@ -103,7 +103,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.project_name}-${environment}-private-data-az1"
+    Name = "${var.project_name}-${var.environment}-private-data-az1"
   }
 }
 
@@ -115,6 +115,6 @@ resource "aws_subnet" "private_data_subnet_az2" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.project_name}-${environment}-private-data-az2"
+    Name = "${var.project_name}-${var.environment}-private-data-az2"
   }
 }
